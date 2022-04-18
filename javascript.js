86a340853de0341f60a28633cdcc63c2e8507ec4
Steven <steven.guzman@yachaytech.edu.ec>
2022-04-18 01:50:52 -0500

    Added playRound function This function takes two parameters: the user's choice and the computer's choice. Then, it verifies if the user won or lose and returns the result.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Gets a random word between rock, papers, scissors.
 function computerPlay() {
     //Create a list of the names.
     const list = ["Rock", "Paper", "Scissors"];
@@ -13,3 +14,50 @@ function computerPlay() {
     return name;
 }
 
+//This function plays the game of rock papers scissors
+//between the two given competitors.
+function playRound(playerSelection, computerSelection) {
+    //Rock wins against scissors.
+    //Scissors win against paper.
+    //Paper wins against rock.
+    
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Won! Rock beats scissors.";
+    }
+
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You won! Scissors beat paper.";
+    }
+
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Won! Paper beats rock."
+    }
+
+    if (computerSelection == "rock" && playerSelection == "scissors") {
+        return "You Lose! Rock beats scissors.";
+    }
+
+    else if (computerSelection == "scissors" && playerSelection == "paper") {
+        return "You lose! Scissors beat paper.";
+    }
+
+    else if (computerSelection == "paper" && playerSelection == "rock") {
+        return "You Lose! Paper beats rock."
+    }
+    
+    else if (computerSelection == playerSelection) {
+        return "It's a draw!";
+    }
+
+}
+
+let user = "Rock";
+console.log(`You chose ${user}.`);
+const computer = computerPlay();
+console.log(`Computer chose ${computer}.`);
+
+const game = playRound(user, computer);
+console.log(game);
