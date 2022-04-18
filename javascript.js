7b54c1b2e151fd292688305730a54bfa23fe647a
Steven <steven.guzman@yachaytech.edu.ec>
2022-04-18 01:04:56 -0500

    Added computerPlay function This function takes a random word between rock paper scissors.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello, bitch.");
+function computerPlay() {
+    //Create a list of the names.
+    const list = ["Rock", "Paper", "Scissors"];
+
+    //I have to get a random variable from my list of names.
+    //For this, I have to create a range from zero to the 
+    //length of the list minus 1. Then, I have to pick a random
+    //number between this range, and use it as an index for the 
+    //list, and in this way, I can choose a random variable.
+    let i = Math.floor(Math.random() * (list.length));
+    let name = list[i];
+    
+    return name;
+}
+
